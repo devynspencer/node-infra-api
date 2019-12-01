8b92373fb5487d09c19e56b2ac60416b3e8df99d
Devyn Spencer <devynspencer@users.noreply.github.com>
2019-12-01 13:56:46 -0800

    Add tests for new server endpoints

--- src/servers/servers.controller.spec.ts
+++ src/servers/servers.controller.spec.ts
@@ -14,7 +14,23 @@ describe('ServersController', () => {
 
   describe('/servers', () => {
     it('returns all servers', () => {
-      expect(serversController.findAll()).toBe('All servers');
+      expect(serversController.findAll()).toBe('All servers returned');
+    });
+
+    it('returns a server by id', () => {
+      expect(serversController.find({ id: 2 })).toBe('Server 2 returned');
+    });
+
+    it('adds a server', () => {
+      expect(serversController.add({ hostname: 'srv03' })).toBe('Server srv03 added');
+    });
+
+    it('removes a server by id', () => {
+      expect(serversController.remove({ id: 4 })).toBe('Server 4 removed');
+    });
+
+    it('updates a server', () => {
+      expect(serversController.update({ id: 5, hostname: 'srv05' })).toBe('Server 5 updated');
     });
   });
 });
