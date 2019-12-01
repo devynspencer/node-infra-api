b953e08130446599c528dd0294100965954f5daa
Devyn Spencer <devynspencer@users.noreply.github.com>
2019-12-01 13:56:34 -0800

    Add server endpoints for common actions

--- src/servers/servers.controller.ts
+++ src/servers/servers.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Param, Delete, Put } from '@nestjs/common';
 
 @Controller('servers')
 export class ServersController {
   @Get()
   findAll(): string {
-    return 'All servers';
+    return 'All servers returned';
+  }
+
+  @Get(':id')
+  find(@Param() params) {
+    return `Server ${params.id} returned`;
+  }
+
+  @Post()
+  add(@Param() params): string {
+    return `Server ${params.hostname} added`;
+  }
+
+  @Delete(':id')
+  remove(@Param() params): string {
+    return `Server ${params.id} removed`;
+  }
+
+  @Put(':id')
+  update(@Param() params): string {
+    return `Server ${params.id} updated`;
   }
 }
